c6eef6b7079ef29abf339bb657b54eca4da238a1
guy187 <guylherme699@gmail.com>
2024-04-08 17:30:22 -0300

    Criado projeto orientado a objetos com o JS e Firebase

---
+++ js/configure.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTO9xVse7Yqm86xmvCoFruXBj9-kIEm58",
+    authDomain: "lista-fdb.firebaseapp.com",
+    projectId: "lista-fdb",
+    storageBucket: "lista-fdb.appspot.com",
+    messagingSenderId: "844822910694",
+    appId: "1:844822910694:web:ece2dac02d586bdeb54d27",
+    measurementId: "G-NSC57ML4FJ"
+  };
